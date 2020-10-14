eeadbfe0dec5946acb7945254ab77b15e61b866c
julry <julry6999@gmail.com>
2020-10-14 16:27:31 +0300

    Update build

---
+++ build/precache-manifest.364d25cd48a3b5e6305e3e70d642fddc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59068093e98b39f75d050462b5158f05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1937345b0aa4823d42e",
+    "url": "/static/js/2.01ed5315.chunk.js"
+  },
+  {
+    "revision": "863004d46cd7375f6c043c934b2ed4e3",
+    "url": "/static/js/2.01ed5315.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0b3495e638e72b25313",
+    "url": "/static/js/main.0e515091.chunk.js"
+  },
+  {
+    "revision": "e1b96b7066e5af263c65",
+    "url": "/static/js/runtime-main.351f67a5.js"
+  }
+]);
